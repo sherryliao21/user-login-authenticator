6173f257455483f226cff6ff1092f407fb6306e7
Sherry <sherry.c.liao@gmail.com>
2021-01-30 23:34:44 +0800

    feat: add user email & password authentication

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/login-authentication', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
+const UserInfo = require('./models/userInfo')
 
 db.on('error', () => {
   console.log('MongoDB error!')
@@ -19,8 +20,15 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-// app.post('/', (req, res) => {
-// })
+app.get('/user_authentication', (req, res) => {
+  const email = req.query.email
+  const password = req.query.password
+  UserInfo.find({ email: email, password: password })
+    .lean()
+    .then(user =>
+      res.render('welcome', { userName: user[0].firstName }))
+    .catch(error => res.redirect('/'))
+})
 
 app.listen(3000, (req, res) => {
   console.log('Express is listening on localhost: 3000')
